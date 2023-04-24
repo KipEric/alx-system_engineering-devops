e6ab238d16137042c8c29d2942ab013112665ba2
KipEric <kipkoecheric97@gmail.com>
2023-04-25 02:56:09 +0300

    Task 0 correction

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,28 +4,18 @@
 import requests
 from sys import argv
 
-if __name__ == "__main__":  
-
-
+if __name__ == "__main__":
     if len(argv) != 2:
         print(f"Usage: {argv[0]} EMPLOYEE_ID")
         sys.exit(1)
-
-
     EMPLOYEE_ID = argv[1]
     API_URL = "https://jsonplaceholder.typicode.com"
-
-
     employee_response = requests.get(f"{API_URL}/users/{EMPLOYEE_ID}")
     employee_response.raise_for_status()
     employee_name = employee_response.json()["name"]
-
-
     task_response = requests.get(f"{API_URL}/todos?userId={EMPLOYEE_ID}")
     task_response.raise_for_status()
     task = task_response.json()
-
-
     total_tasks = len(task)
     done_tasks = len([todo for todo in task if todo["completed"]])
 
