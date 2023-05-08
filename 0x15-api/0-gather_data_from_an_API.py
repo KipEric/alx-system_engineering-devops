9231293a52ada054bffeb69a5687cc38bc55ff15
KipEric <kipkoecheric97@gmail.com>
2023-05-08 19:00:33 +0300

    Task 0 corr

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-"""A python script that for a given employee id, returns information about his /her TODO list progress"""
+"""
+A python script that for a given employee id,
+returns information about his /her TODO list progress
+"""
 
 from sys import argv
 import requests
@@ -9,29 +12,26 @@ if __name__ == "__main__":
     if len(argv) != 2:
         print(f"Usage: {argv[0]} EMPLOYEE_ID")
         sys.exit(1)
-    EMPLOYEE_ID = argv[1]
-    API_URL = "https://jsonplaceholder.typicode.com"
-    
+    employee_id = argv[1]
+    api_url = "https://jsonplaceholder.typicode.com"
 
     """Employees name"""
-    employee_response = requests.get(f"{API_URL}/users/{EMPLOYEE_ID}")
+    employee_response = requests.get(f"{api_id}/users/{employee_id}")
     employee_response.raise_for_status()
     employee_name = employee_response.json()["name"]
-    
-    
+
     """The employees task list"""
-    task_response = requests.get(f"{API_URL}/todos?userId={EMPLOYEE_ID}")
+    task_response = requests.get(f"{api_url}/todos?userId={employee_id}")
     task_response.raise_for_status()
-    task = task_response.json()
-    
+    tasks = task_response.json()
 
     """Calculation of task list progress"""
-    total_tasks = len(task)
-    done_tasks = len([todo for todo in task if todo["completed"]])
-
+    total_tasks = len(tasks)
+    completed_tasks = [task for task in tasks if task["completed"]]
+    completed_task_count = len(completed_tasks)
 
     """Print the progress report"""
-    print(f"Eployee {employee_name} is done with tasks ({done_tasks}/{total_tasks}):")
-    for todo in task:
-        if todo["completed"]:
-            print(f"\t{todo['title']}")
+    print(f"Employee {employee_name} is done with tasks"
+          f"({completed_task_count}/{total_tasks}):")
+    for task in completed_tasks:
+        print(f"\t{task['title']}")
