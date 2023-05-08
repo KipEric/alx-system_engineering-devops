c79e0445fb73be530240a90940e9dce7098b465f
KipEric <kipkoecheric97@gmail.com>
2023-05-08 21:56:38 +0300

    Task 0 corr1

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,23 +4,26 @@ A python script that for a given employee id,
 returns information about his /her TODO list progress
 """
 
-from sys import argv
+
 import requests
+import sys
+
 
 if __name__ == "__main__":
     """Check if argument 2 is present"""
-    if len(argv) != 2:
-        print(f"Usage: {argv[0]} EMPLOYEE_ID")
+    if len(sys.argv) != 2:
+        print(f"Usage: {sys.argv[0]} EMPLOYEE_ID")
         sys.exit(1)
-    employee_id = argv[1]
+  
+    employee_id = sys.argv[1]
     api_url = "https://jsonplaceholder.typicode.com"
 
-    """Employees name"""
-    employee_response = requests.get(f"{api_id}/users/{employee_id}")
+    """Retrieve Employees name"""
+    employee_response = requests.get(f"{api_url}/users/{employee_id}")
     employee_response.raise_for_status()
     employee_name = employee_response.json()["name"]
 
-    """The employees task list"""
+    """Retriev the employees task list"""
     task_response = requests.get(f"{api_url}/todos?userId={employee_id}")
     task_response.raise_for_status()
     tasks = task_response.json()
@@ -33,5 +36,6 @@ if __name__ == "__main__":
     """Print the progress report"""
     print(f"Employee {employee_name} is done with tasks"
           f"({completed_task_count}/{total_tasks}):")
+    
     for task in completed_tasks:
         print(f"\t{task['title']}")
